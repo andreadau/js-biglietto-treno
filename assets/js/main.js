29edc0394d273b05cbe7d443ca5b1e2876df8337
Andrea <dauriaandrea99@gmail.com>
2020-11-13 15:54:43 +0100

    € add ! END !

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,11 +8,11 @@ eta = prompt("Quanti anni hai ?");
 var biglietto = numKm * 0.21;
 // Condizione di non sconto sul biglietto 
 if (eta < 18) {
-    document.getElementById('costo').innerHTML = biglietto - (biglietto * 0.2);
+    document.getElementById('costo').innerHTML = biglietto - (biglietto * 0.2) + "€";
 } else if (eta >= 18 && eta < 65) {
-    document.getElementById('costo').innerHTML = biglietto;
+    document.getElementById('costo').innerHTML = biglietto + "€";
 } else {
-    document.getElementById('costo').innerHTML = biglietto - (biglietto * 0.4);
+    document.getElementById('costo').innerHTML = biglietto - (biglietto * 0.4) + "€";
 }
 
  
